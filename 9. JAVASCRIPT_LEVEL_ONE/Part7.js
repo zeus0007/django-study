519452966e780152d25f73bee0d2ff2e6d4e6e21
Jewoo <wpdn79@likelion.org>
2020-05-27 18:55:25 +0900

    57 JS Level One - Part Seven ForLoops

---
+++ 9. JAVASCRIPT_LEVEL_ONE/Part7.js
@@ -0,0 +1,21 @@
+// for ( var i = 0; i < array.length; i++){
+//     array[i];
+// }
+
+// for loops with number
+// for (var num = 0; num < 5; num++) {
+//   console.log("Number is " + num);
+// }
+
+// for loops with length
+// var word = "ABCDEFGHIJK";
+
+// for (var i = 0; i < word.length; i++) {
+//   console.log(word[i]);
+// }
+
+// var word = "ABABABABAB";
+
+// for (var i = 0; i < word.length; i = i + 2) {
+//   console.log(word[i]);
+// }

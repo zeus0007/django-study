64c848918b17c7aa080161919e715d1f3adcde71
Jewoo <wpdn79@likelion.org>
2020-05-27 14:08:41 +0900

    50 JS Level One - Part One Basics

---
+++ 9. JAVASCRIPT_LEVEL_ONE/Part1.js
@@ -0,0 +1,50 @@
+alert("Hello World!"); // 화면 알림창
+// 이것은 주석
+// 데이터 타입
+//number
+10;
+20.2 - 13.4;
+//string
+("Hello World!");
+("10");
+//Boolenas
+truefalse;
+//undefined and null
+undefined;
+null;
+clear; //콘솔 지우기
+//연산
+2 + 2;
+5 - 1;
+3 * 2;
+10 / 2;
+2 / 5;
+2 ** 4; //제곱
+15 / 14;
+15 % 14; //1 나머지
+clear; // 콘솔지우기
+//STRINGS
+"Django" + " is cool";
+"django".length; //글자수 -> 띄어쓰기도 포함
+"x x".length;
+("hello \n start a new line"); // \n은 엔터
+("hello \t give me a tab"); // \t는 탭
+('hello "quotes " '); // \"는 따옴표
+"hello"[0]; // 문자열의 첫번째 글자 , 0부터 시작함을 알려줌
+clear;
+//VARIABLES
+var varName = value; // varibale name
+var bankAccount = 100;
+backAccount;
+var deposit = 50;
+var total = bankAccount + deposit;
+total;
+var greeting = "Welcome back: ";
+var name = "Jose";
+alert(greeting + name);
+var myVariable; // undefined 선언은 했지만 정의되지 않은 경우
+//null은 null이라는 아무것도 없다는 값을 가지고 있는것.
+var bonus = null;
+console.log("Hey I'm in the console"); //콘솔에 입력
+prompt("Enter somethig"); //입력 받는 브라우저 알림창
+var age = prompt("Enter your age"); //요런식으로 사용함.

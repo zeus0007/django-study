0c23a764af04151434a620d100969fed98c74334
Jewoo <wpdn79@likelion.org>
2020-05-27 18:43:33 +0900

    56 JS Level One - Part Six WhileLoops

---
+++ 9. JAVASCRIPT_LEVEL_ONE/Part6.js
@@ -0,0 +1,32 @@
+// var x = 0;
+
+// while (x < 5) {
+//   console.log("x is currently: " + x);
+
+//   if (x === 3) {
+//     console.log("X IS EQUEAL TO THREE!");
+//     break;
+//   }
+
+//   console.log("x is still less than 5, adding 1 to x");
+
+//   x = x + 1;
+// }
+
+/*excercise 내코드
+var x = 0;
+
+while (x < 10) {
+  x = x + 2;
+  console.log(x);
+}*/
+//excercise 솔루션
+
+var num = 1;
+
+while (num < 11) {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+  num = num + 1;
+}

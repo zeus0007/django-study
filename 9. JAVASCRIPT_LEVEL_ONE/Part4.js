08cdaf86f338a806ca59f2f641aff05be4a659cf
Jewoo <wpdn79@likelion.org>
2020-05-27 17:48:13 +0900

    54 JS Level One - Part Four -Operators

---
+++ 9. JAVASCRIPT_LEVEL_ONE/Part4.js
@@ -0,0 +1,50 @@
+true;
+false;
+
+3 > 2;
+2 > 3;
+
+1 < 2;
+
+2 >= 2;
+
+1 <= 3;
+
+2 == 2;
+
+"2" == 2;
+
+"2" === 2;
+
+"2" !== 2;
+
+true == 1; //true
+true === 1; //false
+
+null == undefined; // true
+NaN == NaN; // false
+
+1 === 1 && 2 === 2; //and operator
+
+1 === 2 || 1 === 1; //or operator
+
+!(1 === 1); //not operator
+
+//excercise
+var x = 1;
+var y = 2;
+
+// Exercise 1
+"2" == y && x === 1;
+
+// Exercise 2
+x >= 0 || y === "2";
+
+// Exercise 3
+!(x !== 1) && y === 1 + 1;
+
+// Exercise 4
+y !== "2" && x < 10;
+
+// Exercise 5
+y !== x || y == "2" || x === 3;

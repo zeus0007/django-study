b14359c79f21eabc03da72f9cc0b8a4d735d64be
Jewoo <wpdn79@likelion.org>
2020-05-30 15:33:32 +0900

    91 Part One Numbers

---
+++ 13. PYTHON_LEVLE_ONE/Part1.py
@@ -0,0 +1,22 @@
+print("hello world")
+
+2+1
+2*2
+2/2
+2-1
+
+print(2+1)
+
+2**5  # 제곱
+4**0.5  # 소수점을 사용한 제곱도 가능
+print(2+10 * 10+3)
+# 파이썬은 데이터 타입을 작성해줄 필요가 없다.
+a = 5
+a = a*5
+print(a)
+# comment
+
+my_income = 100
+tax_rate = 0.1
+my_taxed = my_income * tax_rate
+print(my_taxed)  # float형이 된다.

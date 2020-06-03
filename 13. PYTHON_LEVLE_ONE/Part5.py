25837d70469f4ad04b22d0f709647cd757325f6c
Jewoo <wpdn79@likelion.org>
2020-06-03 09:26:14 +0900

    95 Part 5 - Tuples, Sets, and Booleans

---
+++ 13. PYTHON_LEVLE_ONE/Part5.py
@@ -0,0 +1,22 @@
+# Booleans
+True
+False
+0
+1
+# Tuples
+t = (1, 2, 3)
+print(t[0])
+# Sets
+
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(4)
+x.add(4)
+x.add(0.1)
+print(x)
+
+converted = set([1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3])
+print(converted)

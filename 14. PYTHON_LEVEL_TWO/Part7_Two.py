8bf1f8d3fe9fa54d1d11e469d2075dbc56810c86
Jewoo <wpdn79@likelion.org>
2020-06-09 13:57:59 +0900

    115 Name and Main & 116 Django Level One - Part One -Django Setup

---
+++ 14. PYTHON_LEVEL_TWO/Part7_Two.py
@@ -0,0 +1,9 @@
+import Part7_One
+print("TOP LEVEL TWO.PY")
+Part7_One.func()
+
+
+if __name__ == '__main__':
+    print("TWO.PY is being run directly")
+else:
+    print("TWO.PY has been imported")
---
+++ 15. DJANGO_LEVEL_ONE/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\wpdn7\desktop\udemy_django\15. django_level_one\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

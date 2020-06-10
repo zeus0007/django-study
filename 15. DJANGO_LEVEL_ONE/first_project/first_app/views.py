013c00351a2bc0780b73ae7aecc4ab082de0415b
Jewoo <wpdn79@likelion.org>
2020-06-10 19:24:29 +0900

    122 Django Level One - Part Seven -Templates

--- 15. DJANGO_LEVEL_ONE/first_project/first_app/views.py
+++ 15. DJANGO_LEVEL_ONE/first_project/first_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dict = {'insert_me': "Now I am coming From first_app/views.py!"}
+    return render(request, 'first_app/index.html', context=my_dict)
+    # return HttpResponse("Hello World!")

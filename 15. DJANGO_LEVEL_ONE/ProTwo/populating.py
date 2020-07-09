60a08376555e854aa78eaa3ea6aab89105982c8d
Jewoo <wpdn79@likelion.org>
2020-07-09 16:26:31 +0900

    132 Django Level Two Project Exercise Solution

--- 15. DJANGO_LEVEL_ONE/ProTwo/populating.py
+++ 15. DJANGO_LEVEL_ONE/ProTwo/populating.py
@@ -12,8 +12,9 @@ fakergen = Faker()
 def populate(N = 5):
 
     for entry in range(N):
-        first_name = fakergen.user_name()
-        last_name = fakergen.user_name()
+        name = fakergen.name().split()
+        first_name = name[0]
+        last_name = name[1]
         email = fakergen.email()
         userModel = User.objects.get_or_create(first_name = first_name, last_name = last_name, email = email)
 
